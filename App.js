366b5f9e3c660551012c258ea093fc7c6e51bf7a
jessclinicadev <jess@clinica.dev>
2021-08-25 08:25:53 +0800

    Added local push notification

--- App.js
+++ App.js
@@ -25,74 +25,97 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 import messaging from '@react-native-firebase/messaging';
-import {create} from 'react-test-renderer';
+import PushNotification, {Importance} from "react-native-push-notification";
+import PushNotificationIOS from "@react-native-community/push-notification-ios";
+
+
 
 const App = () => {
 
-  // const createChannel = () => {
-  //   const channel = new firebase.notifications.Android.Channel(
-  //     'channelId',
-  //     'Channel Name',
-  //     firebase.notifications.Android.Importance.Max
-  //   ).setDescription('A natural description of the channel');
-  //   firebase.notifications().android.createChannel(channel);
-  // }
-  // // * Foreground Notification
+  useEffect(() => {
+    PushNotification.createChannel(
+      {
+        channelId: "channelid",
+        channelName: "App Notification",
+        importance: Importance.HIGH,
+        vibrate: true,
+      }
+    );
+    PushNotification.configure({
 
-  // const notificationListener = () => {
-  //   firebase.notifications().onNotification((notification) => {
-  //     if (Platform.OS === 'android') {
+      // onRegister: function (token) {
+      //   console.log('TOKEN:', token);
+      // },
 
-  //       const localNotification = new firebase.notifications.Notification({
-  //         sound: 'default',
-  //         show_in_foreground: true,
-  //       })
-  //         .setNotificationId(notification.notificationId)
-  //         .setTitle(notification.title)
-  //         .setSubtitle(notification.subtitle)
-  //         .setBody(notification.body)
-  //         .setData(notification.data)
-  //         .android.setChannelId('channelId') // e.g. the id you chose above
-  //         .android.setSmallIcon('ic_stat_notification') // create this icon in Android Studio
-  //         .android.setColor('#000000') // you can set a color here
-  //         .android.setPriority(firebase.notifications.Android.Priority.High);
+      onNotification: function (notification) {
+        console.log('NOTIFICATION:', notification);
 
-  //       firebase.notifications()
-  //         .displayNotification(localNotification)
-  //         .catch(err => console.error(err));
+        if (notification.userInteraction) {
+          console.log(notification.data);
+        }
 
-  //     } else if (Platform.OS === 'ios') {
+        notification.finish(PushNotificationIOS.FetchResult.NoData);
+      },
+      // // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
+      // onAction: function (notification) {
+      //   console.log("ACTION:", notification.action);
+      //   console.log("NOTIFICATION:", notification);
+      //   // process the action
+      // },
+      // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+      // onRegistrationError: function (err) {
+      //   console.error(err.message, err);
+      // },
+      // IOS ONLY (optional): default: all - Permissions to register.
+      permissions: {
+        alert: true,
+        badge: true,
+        sound: true,
+      },
+      // Should the initial notification be popped automatically
+      // default: true
+      popInitialNotification: true,
+      /**
+       * (optional) default: true
+       * - Specified if permissions (ios) and token (android and ios) will requested or not,
+       * - if not, you must call PushNotificationsHandler.requestPermissions() later
+       * - if you are not using remote notification or do not have Firebase installed, use this:
+       *     requestPermissions: Platform.OS === 'ios'
+       */
+      requestPermissions: true,
+    });
 
-  //       const localNotification = new firebase.notifications.Notification()
-  //         .setNotificationId(notification.notificationId)
-  //         .setTitle(notification.title)
-  //         .setSubtitle(notification.subtitle)
-  //         .setBody(notification.body)
-  //         .setData(notification.data)
-  //         .ios.setBadge(notification.ios.badge);
+  }, []);
 
-  //       firebase.notifications()
-  //         .displayNotification(localNotification)
-  //         .catch(err => console.error(err));
-
-  //     }
-  //   });
-  // }
-
-  // useEffect(() => {
-  //   createChannel();
-  //   notificationListener();
-  // }, []);
 
   messaging().setBackgroundMessageHandler(async remoteMessage => {
     // triggered when app is closed and in background
     console.log('setBackgroundMessageHandler!', remoteMessage);
   });
 
+
   useEffect(() => {
     requestUserPermission();
     const unsubscribe = messaging().onMessage(async remoteMessage => {
-      Alert.alert('onMessage!', JSON.stringify(remoteMessage));
+      //Alert.alert('onMessage!', JSON.stringify(remoteMessage));
+
+      PushNotification.localNotification({
+        channelId: "channelid",
+        title: remoteMessage.notification.title,
+        message: remoteMessage.notification.body,
+        data: remoteMessage.data,
+
+        // largeIcon: "ic_launcher", // (optional) default: "ic_launcher". Use "" for no large icon.
+        // largeIconUrl: "https://www.fnordware.com/superpng/pnggrad8rgb.jpg", // (optional) default: undefined
+        // smallIcon: "ic_notification", // (optional) default: "ic_notification" with fallback for "ic_launcher". Use "" for default small icon.
+        // bigText: "My big text that will be shown when notification is expanded", // (optional) default: "message" prop
+        // subText: "This is a subText", // (optional) default: none
+        // bigPictureUrl: "https://www.fnordware.com/superpng/pnggrad8rgb.jpg", // (optional) default: undefined
+        // bigLargeIcon: "ic_launcher", // (optional) default: undefined
+        // bigLargeIconUrl: "https://www.fnordware.com/superpng/pnggrad8rgb.jpg", // (optional) default: undefined
+
+      });
+
     });
     return unsubscribe;
   }, []);
@@ -144,7 +167,7 @@ const App = () => {
   getFcmToken = async () => {
     const fcmToken = await messaging().getToken();
     if (fcmToken) {
-      alert(fcmToken);
+      // alert(fcmToken);
       console.log("Your Firebase Token is:", fcmToken);
     } else {
       console.log("Failed", "No token received");
@@ -167,6 +190,7 @@ const App = () => {
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
+
               <Text style={styles.sectionDescription}>
                 Edit <Text style={styles.highlight}>App.js</Text> to change this
                 screen and then come back to see your edits.
